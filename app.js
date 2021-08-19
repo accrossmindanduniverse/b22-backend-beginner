c24b4683d8363d96f922b0ba3631e1db89d3e144
accrossmindanduniverse <fajarmccartney@gmail.com>
2021-08-19 21:02:45 +0700

    add refresh token and chat feature

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const express = require('express')
 const app = express()
 const env = process.env
 const port = env.PORT || 3001
-const connection = require('./src/helpers/db')
+const { db } = require('./src/helpers/db')
 const routes = require('./src/routes')
 const bodyParser = require('body-parser')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+})
 const cors = require('cors')
+const socket = require('./src/middlewares/socket')
+
+io.on('connection', () => {
+  console.log('socket connection is exists')
+})
 
 app.use(cors())
+
+app.use(socket(io))
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -16,13 +29,22 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 
 app.use('/', routes)
+
+// picture
 app.use(env.APP_UPLOAD_ROUTE, express.static(env.APP_UPLOAD_PATH))
 
-app.listen(port, () => {
+app.get('/files/:file', function (req, res) {
+  return res.download('./assets/files/' + req.params.file)
+})
+
+// file
+app.use(env.APP_FILE_ROUTE, express.static(env.APP_UPLOAD_FILE_PATH))
+
+server.listen(port, () => {
   console.log(`App running on port ${port}`)
 })
 
-connection.connect(function (err) {
+db.connect(function (err) {
   if (err) {
     throw err
   } else {
